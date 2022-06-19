0e32f94a4f01b849dd3e71eece51c1abd8fbe62b
CrossingStiles <CrossingStiles@gmail.com>
2022-06-19 10:25:10 -0400

    Removed date lines on price chart

--- charts.py
+++ charts.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import altair as alt
 from data_fetchers import price_fetch
-from datetime import datetime
 import pandas as pd
 
 
@@ -52,34 +51,7 @@ def price_chart(token1, token2, lookback, target, creation, expiry):
         size=alt.value(2)
     )
 
-    expiry = datetime.strptime(expiry, "%Y-%m-%dT%H:%M:%S")
-    if prices["Timestamp"].iloc[0] < expiry <= datetime.utcnow():
-        expiry_line = base.mark_rule().encode(x=alt.datum(alt.DateTime(
-            year=expiry.year, month=expiry.month, date=expiry.day, hours=expiry.hour, minutes=expiry.minute,
-            seconds=expiry.second)))
-    else:
-        expiry_line = []
-
-    creation = datetime.strptime(creation, "%Y-%m-%dT%H:%M:%S")
-    if prices["Timestamp"].iloc[0] < creation:
-        creation_line = base.mark_rule().encode(x=alt.datum(alt.DateTime(
-            year=creation.year, month=creation.month, date=creation.day, hours=creation.hour,
-            minutes=creation.minute,
-            seconds=creation.second)))
-    else:
-        creation_line = []
-
-    if expiry_line:
-        if creation_line:
-            st.altair_chart((line + points + price_target + tooltips + creation_line + expiry_line),
-                            use_container_width=True)
-        else:
-            st.altair_chart((line + points + price_target + tooltips + expiry_line), use_container_width=True)
-    else:
-        if creation_line:
-            st.altair_chart((line + points + price_target + creation_line + tooltips), use_container_width=True)
-        else:
-            st.altair_chart((line + points + price_target + tooltips), use_container_width=True)
+    st.altair_chart((line + points + price_target + tooltips), use_container_width=True)
 
 
 def size_pie_chart(order_data):
